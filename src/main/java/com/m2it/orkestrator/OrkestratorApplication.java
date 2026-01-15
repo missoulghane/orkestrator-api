751b91f0174c06d3d1d913889aeebc54647cb406
missoulghane <m.issoulghane@gmail.com>
2026-01-15 11:36:25 +0100

    Init solution

---
+++ src/main/java/com/m2it/orkestrator/OrkestratorApplication.java
@@ -0,0 +1,13 @@
+package com.m2it.orkestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrkestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrkestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2it/orkestrator/OrkestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2it.orkestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrkestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
